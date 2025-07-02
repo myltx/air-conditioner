4afbb275d7484a64f58475edf2fd0f18458fde4d
myltx <summmayl@gmail.com>
2025-07-02 15:32:38 +0800

    feat: 更新环境变量配置，使用 VITE_BASE_PATH 替代 VITE_PUBLIC_PATH

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BASE_PATH: string;
+  // 其他自定义环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import UnoCSS from "unocss/vite";
 import { presetUno, presetAttributify, presetIcons } from "unocss";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.VITE_PUBLIC_PATH || "/",
-  plugins: [
-    react(),
-    UnoCSS({
-      presets: [
-        presetUno(),
-        presetAttributify(),
-        presetIcons({
-          scale: 1.2,
-          cdn: "https://esm.sh/",
-        }),
-      ],
-      shortcuts: {
-        "flex-center": "flex items-center justify-center",
-      },
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  console.log("VITE_BASE_PATH:", env);
+  return {
+    base: env.VITE_BASE_PATH || "/",
+    plugins: [
+      react(),
+      UnoCSS({
+        presets: [
+          presetUno(),
+          presetAttributify(),
+          presetIcons({
+            scale: 1.2,
+            cdn: "https://esm.sh/",
+          }),
+        ],
+        shortcuts: {
+          "flex-center": "flex items-center justify-center",
+        },
+      }),
+    ],
+  };
 });
